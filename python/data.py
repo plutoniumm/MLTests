1e36387469b91ee61d623e153c0207823a71440a
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-11-24 14:16:29 +0530

    11% REGR lmao

--- python/data.py
+++ python/data.py
@@ -7,7 +7,7 @@ def Scale(data):
     scaler = MinMaxScaler()
 
     d_scaled = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-    d_scaled = d_scaled[:1000]  # Temporary only 1000
+    # d_scaled = d_scaled[:1000]  # Temporary only 1000
     # Convert to float32 from 64 to increase speed
     d_scaled = d_scaled.astype('float32')
     return d_scaled
